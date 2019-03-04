1555866853f8fbcab9869e43490d73b0cc475e27
siliconedge <misha@mishasmail.com>
2019-03-04 14:11:23 +1100

    Menu mesh and cogic WIP

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -5,27 +5,24 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
 
-    MenuUIManager UI = null;
+    public TextMesh[] ScoreTexts; 
 
     public void Reset(){
         
     }
 
     public void SetScores(int? score, State state, int indexOfScoreOnLedderboard){
-        UI = GetComponentInChildren<MenuUIManager>();
         
         if (score.HasValue){
             if (indexOfScoreOnLedderboard == 0){
-                UI.SetCurrentScore("New Record!", score.Value);
+                //UI.SetCurrentScore("New Record!", score.Value);
             }
             else if (indexOfScoreOnLedderboard < 10){
-                UI.SetCurrentScore("New Top 10", score.Value);
+                //UI.SetCurrentScore("New Top 10", score.Value);
             }
             else{
-                UI.SetCurrentScore("Your Score", score.Value);
+                //UI.SetCurrentScore("Your Score", score.Value);
             }
         }
-        
-        UI.SetHighScores(state, indexOfScoreOnLedderboard);
 	}
 }
